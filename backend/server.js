934dd2662bbf50de9113a133cd3e3c2cb09df05c
shashwat1211 <shashwatraj30@gmail.com>
2023-07-16 23:06:18 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors")
 const routes = require("./routes/blogs")
  const userRoutes = require("./routes/user")
-
+PORT = process.env.PORT || 3005
 const app = express();
 const mongoose = require("mongoose");
 
-
+app.use(cors())
 app.use((req,res,next)=>{
     next()
 })
@@ -16,7 +16,7 @@ app.use("/api/blogs" , routes);
  app.use("/api/user", userRoutes);
 mongoose.connect(process.env.MONG_URI)
     .then(()=>{
-        app.listen(process.env.PORT || 3005, () => { console.log("Database is connected & Server is running") });
+        app.listen(PORT , () => { console.log("Database is connected & Server is running") });
     })
     .catch(e=>{console.log(e)});
 
